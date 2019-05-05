e0b7e95b6eb85d1ca93c0fe5b1e41d70762f0320
gokernel2017 <31101188+gokernel2017@users.noreply.github.com>
2019-05-05 19:43:39 -0300

    Create kbhit.c

---
+++ kbhit.c
@@ -0,0 +1,45 @@
+/**
+ Linux (POSIX) implementation of _kbhit().
+ Morgan McGuire, morgan@casual-effectcs.com
+ */
+#include <stdio.h>
+#include <sys/ioctl.h>
+#include <sys/select.h>
+#include <termios.h>
+#include <stropts.h>
+
+int _kbhit() {
+    static const int STDIN = 0;
+    static bool initialized = false;
+
+    if (! initialized) {
+        // Use termios to turn off line buffering
+        termios term;
+        tcgetattr(STDIN, &term);
+        term.c_lflag &= ~ICANON;
+        tcsetattr(STDIN, TCSANOW, &term);
+        setbuf(stdin, NULL);
+        initialized = true;
+    }
+
+    int bytesWaiting;
+    ioctl(STDIN, FIONREAD, &bytesWaiting);
+    return bytesWaiting;
+}
+
+//////////////////////////////////////////////
+//    Simple demo of _kbhit()
+
+#include <unistd.h>
+
+int main(int argc, char** argv) {
+    printf("Press any key");
+    while (! _kbhit()) {
+        printf(".");
+        fflush(stdout);
+        usleep(1000);
+    }
+    printf("\nDone.\n");
+
+    return 0;
+}
